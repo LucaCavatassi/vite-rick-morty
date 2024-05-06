8eff9f36f469241d430f3d07bcb57223aff680e1
LucaCavatassi <lucamariacavatassi@gmail.com>
2024-05-06 16:05:50 +0200

    feat: printed

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script>
-  export default {
-    
-  }
+import axios from "axios";
+import AppHeader from './components/AppHeader.vue';
+import AppCards from "./components/AppCards.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppCards
+  },
+  
+  data () {
+    return {
+      charactersArray: [],
+    };
+  },
+
+  created() {
+    axios.get("https://rickandmortyapi.com/api/character").then((resp)=> {
+      console.log(resp);
+      this.charactersArray = resp.data.results
+      console.log(this.charactersArray);
+    });
+  },
+
+}
 
 </script>
 
 <template>
+  <AppHeader /> 
+  <div class="container">
+    <div class="row">
+        <AppCards :charactersArray ="charactersArray"/>
+    </div>
+  </div>
 
 </template>
 
